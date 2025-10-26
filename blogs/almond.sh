7207dae633f848123d5ebf7dbafc355be6a15a08
ojitha <ojithak@gmail.com>
2025-10-26 15:41:22 +1100

    add the git to the jupyter and remove the almond docker script

--- blogs/almond.sh
+++
@@ -1,4 +0,0 @@
-docker run --name almond -it --rm -p 8888:8888 \
- -v "$(pwd)":/home/jovyan/work \
- -e JUPYTER_CONFIG_DIR="/home/jovyan/work/jupyter_config" \
- -e JUPYTER_TOKEN="almond"  almondsh/almond:latest
